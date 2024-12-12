6e0176b50beb9d44cea00ab976a4e02cf6d6d231
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-12-11 19:21:36 -0500

    Docstrings of module `github_user_repository.py` improved

--- commitfetch/github_user_repository.py
+++ commitfetch/github_user_repository.py
@@ -1,6 +1,7 @@
 """
-This module stores GitHubUser instances for later use.
-It can prevent the creation of many identical instances.
+This module stores GitHubUser instances. It uses their property login as a key
+to grant access to them. Using this module can prevent the creation of many
+identical GitHubUser instances.
 """
 
 
@@ -15,8 +16,8 @@ def get_github_user(user_login):
 		user_login (str): the wanted user's login name.
 
 	Returns:
-		GitHubUser: data about the specified GitHub user or None if the user
-			is not registered.
+		GitHubUser: data about the specified GitHub user, None if the user is
+		not registered.
 	"""
 	github_user = _repository_content.get(user_login)
 	return github_user
@@ -24,7 +25,7 @@ def get_github_user(user_login):
 
 def register_github_user(github_user):
 	"""
-	Registers data about a GitHub user in the repository if the user is not
+	Registers data about a GitHub user in the repository if this user is not
 	registered.
 
 	Parameters:
