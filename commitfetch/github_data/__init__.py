ca877bea637359f49c42c7c763801c059015358b
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2024-12-11 19:55:08 -0500

    Package `github_data` created

---
+++ commitfetch/github_data/__init__.py
@@ -0,0 +1,10 @@
+from .commit import\
+	Commit
+from .github_credentials import\
+	GitHubCredentials
+from .github_user import\
+	GitHubUser
+from . import\
+	github_user_repository
+from .repo_identity import\
+	RepoIdentity
