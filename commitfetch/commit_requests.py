12c8939a3b0f4c24b92433d9205677da221cc54b
Guyllaume Rousseau <guyllaume.rousseau@hotmail.com>
2025-01-06 21:45:49 -0500

    Exception `GitHubAPIError` works.

--- commitfetch/commit_requests.py
+++ commitfetch/commit_requests.py
@@ -5,6 +5,9 @@ import requests
 from time import\
 	sleep
 
+from .github_api_error import\
+	GitHubAPIError
+
 from .github_data import\
 	Commit,\
 	GitHubUser,\
@@ -24,6 +27,7 @@ _KEY_LOGIN = "login"
 _KEY_MESSAGE = "message"
 _KEY_NAME = "name"
 _KEY_SHA = "sha"
+_KEY_STATUS = "status"
 _KEY_URL = "url"
 
 _PATH_COMMITS = "/commits/"
@@ -173,10 +177,10 @@ def _raise_github_api_error(request_url, api_data):
 	if isinstance(api_data, dict):
 		message = api_data.get(_KEY_MESSAGE)
 		doc_url = api_data.get(_KEY_DOCUMENTATION_URL)
+		status = api_data.get(_KEY_STATUS)
 
-		if message is not None and doc_url is not None:
-			exc_msg = f"{message} ({request_url}). Documentation: {doc_url}."
-			raise RuntimeError(exc_msg)
+		if message is not None and doc_url is not None and status is not None:
+			raise GitHubAPIError(message, doc_url, status, request_url)
 
 
 def _repo_from_commit_api_url(url):
---
+++ commitfetch/github_api_error.py
@@ -0,0 +1,58 @@
+# __all__ declared at the module's end
+
+
+class GitHubAPIError(Exception):
+	"""
+	An exception raised when a request to the GitHub API is unsuccessful.
+	"""
+
+	def __init__(self, message, doc_url, status, req_url):
+		"""
+		The constructor needs the data relevant to the request's failure.
+
+		Args:
+			message (str): the error message.
+			doc_url (str): the URL to the error's documentation.
+			status (str): the response's status code.
+			req_url (str): the problematic request's URL.
+		"""
+		self._message = message
+		self._doc_url = doc_url
+		self._status = status
+		self._req_url = req_url
+
+	def __repr__(self):
+		return self.__class__.__name__\
+			+ f"({self._message}, {self._doc_url}, "\
+			+ f"{self._status}, {self._req_url})"
+
+	@property
+	def message(self):
+		"""
+		str: the error message.
+		"""
+		return self._message
+
+	@property
+	def doc_url(self):
+		"""
+		str: the URL to the error's documentation.
+		"""
+		return self._doc_url
+
+	@property
+	def status(self):
+		"""
+		str: the response's status code.
+		"""
+		return self._status
+
+	@property
+	def req_url(self):
+		"""
+		str: the problematic request's URL.
+		"""
+		return self._req_url
+
+
+__all__ = [GitHubAPIError.__name__]
